6c2c41c744660aa875c6603b340be6e8ebc9cae7
Bishal kumar Shah <shahvishal.9090@gmail.com>
2026-03-08 00:11:54 +0530

    chore: delete Unistyles configuration files

--- src/styles/unistyles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native-unistyles";
-import { theme } from "./theme";
-
-// Configure Unistyles v3 themes before any components use StyleSheet.create
-StyleSheet.configure({
-  themes: {
-    light: theme,
-  },
-});
